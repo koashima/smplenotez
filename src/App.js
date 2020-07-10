262aceed1f9572cf748bba4134e319da9b2a6a29
koashima <48111257+koashima@users.noreply.github.com>
2020-07-09 18:20:28 -0700

    adds create note functionality via GraphQL api

--- src/App.js
+++ src/App.js
@@ -1,62 +1,115 @@
-import React, { useEffect, useReducer }from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { API } from 'aws-amplify';
-import { List } from 'antd';
+import { List, Input, Button } from 'antd';
 import 'antd/dist/antd.css';
-import { listNotes } from './graphql/queries'
+import { v4 as uuid } from 'uuid';
+import { listNotes } from './graphql/queries';
+import { createNote as CreateNote } from './graphql/mutations';
 import './App.css';
 
-const initialState = { 
+const CLIENT_ID = uuid();
+
+const initialState = {
   notes: [],
   loading: true,
   error: false,
-  form: { name: '', description: '' }
-}
+  form: { name: '', description: '' },
+};
 
 function reducer(state, action) {
-  switch(action.type) {
+  switch (action.type) {
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes] };
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form };
+    case 'SET_INPUT':
+      return { ...state, form: { ...state.form, [action.name]: action.value } };
     case 'SET_NOTES':
-      return { ...state, notes: action.notes, loading: false }
+      return { ...state, notes: action.notes, loading: false };
     case 'ERROR':
-      return { ...state, loading: false, error: true }
-    default: 
-      return state
+      return { ...state, loading: false, error: true };
+    default:
+      return state;
   }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   async function fetchNotes() {
     try {
       const notesData = await API.graphql({
-        query: listNotes
-      })
-      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items })
+        query: listNotes,
+      });
+      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items });
     } catch (err) {
-      console.log('error: ', err)
-      dispatch({ type: 'ERROR' })
+      console.log('error: ', err);
+      dispatch({ type: 'ERROR' });
     }
   }
 
-  useEffect( () => {
-    fetchNotes()
-  }, [])
+  async function createNote() {
+    const { form } = state;
+    if (!form.name || !form.description)
+      return alert('please enter a name and description');
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() };
+    dispatch({ type: 'ADD_NOTE', note });
+    dispatch({ type: 'RESET_FORM' });
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: { input: note },
+      });
+      console.log('successfully created note!');
+    } catch (err) {
+      console.log('error: ', err);
+    }
+  }
+
+  function onChange(e) {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value });
+  }
 
   function renderItem(item) {
     return (
       <List.Item style={styles.item}>
-        <List.Item.Meta 
+        <List.Item.Meta
           title={item.name}
-          description={item.description} 
+          description={item.description}
+          key={item.id}
         />
       </List.Item>
-    )
+    );
   }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
 
   return (
     <div className="App">
-      <h1 style={{ backgroundColor: 'lightgrey', borderRadius: 5 }}>SMPL NOTEZ</h1>
-      <List 
+      <h1 style={{ backgroundColor: 'lightgrey', borderRadius: 5 }}>
+        SMPL NOTEZ
+      </h1>
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder="NOTE NAME"
+        name="name"
+        style={styles.input}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder="NOTE DESCRIPTION"
+        name="description"
+        style={styles.input}
+      />
+      <Button onClick={createNote} type="primary">
+        CREATE NOTE
+      </Button>
+
+      <List
         loading={state.loading}
         dataSource={state.notes}
         renderItem={renderItem}
@@ -68,6 +121,6 @@ function App() {
 const styles = {
   input: { marginBottom: 10 },
   item: { textAlign: 'left' },
-  p: { color: '#1890ff' }
-}
+  p: { color: '#1890ff' },
+};
 export default App;
